5ab14dc56b1f028d7586d07284c7767b150c0322
Toby Jaffey <toby@ringtailsoftware.co.uk>
2024-12-05 16:57:25 +0000

    Update for new API

--- src/main.zig
+++ src/main.zig
@@ -5,13 +5,16 @@ const c = @cImport({
     @cInclude("main.c");
 });
 
+fn writeFn(data:[]const u8) void {
+    _ = std.io.getStdOut().writer().write(data) catch 0;
+}
+
 pub fn main() !void {
-    const writer = std.io.getStdOut().writer();
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
     // init zepto with a memory allocator and a writer (for stdout and stderr)
-    zeptolibc.init(allocator, writer);
+    zeptolibc.init(allocator, writeFn);
 
     c.cmain();
 }
